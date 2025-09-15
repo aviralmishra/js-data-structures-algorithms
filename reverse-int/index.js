2acfaec048d6acb0556b94e10500b1a10590339a
Aviral Mishra <aviral.mishra@gmail.com>
2025-09-16 09:21:37 +1000

    reverse int

---
+++ reverse-int/index.js
@@ -0,0 +1,15 @@
+/*
+  Given an integer, return a new integer with the reversed order
+  of characters e.g. reverseInt(15) === 51 / reverseInt(-90) === -9
+*/
+
+function reverseInt(n) {
+    const reversed = n.toString().split('').reduce((rev, char) =>
+        char + rev,
+        ''
+    );
+
+    return parseInt(reversed) * Math.sign(n);
+}
+
+module.exports = reverseInt;
