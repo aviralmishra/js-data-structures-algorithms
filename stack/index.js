4c96955bf960188947accac57accfdb96f6e6b04
Aviral Mishra <aviral.mishra@gmail.com>
2025-09-19 15:43:45 +1000

    stack

--- stack/index.js
+++ stack/index.js
@@ -1,6 +1,6 @@
 /*
     --- Directions
-    Create a stack data structure. The stack should be a
+    Create a stack data structure. The stack (FILO) should be a
     class with methods 'push', 'pop', and 'peek'.  
     
     Adding an element to the stack should store it until it is removed.
@@ -13,6 +13,22 @@
     s.pop(); // returns 1
 */
 
-class Stack { }
+class Stack {
+    constructor() {
+        this.data = [];
+    }
+
+    push(record) {
+        this.data.push(record);
+    }
+
+    pop() {
+        return this.data.pop();
+    }
+
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+}
 
 module.exports = Stack;
